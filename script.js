828878519fb89b7fb9dcc09b8f8b2cf3623e759e
Guilherme Jorge Huyer <107578362+ghuyer@users.noreply.github.com>
2023-02-14 16:36:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const secretNumber = Math.round(Math.random() * 10);
+
+let numberOfAttempts = 1;
+
+let failedAttempts = [];
+
+let result = document.getElementById("result");
+
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Enter') {
+    guess()
+  }
+})
+
+
+
+
+
+function guess() {
+  let userChoice = document.querySelector("input").value;
+  userChoice = Number(userChoice);
+
+  if (userChoice == "" || userChoice < 0 || userChoice > 10) {
+    alert("Você precisa escolher um número entre 0 e 10.");
+    document.querySelector("input").value = "";
+    document.querySelector("input").focus();
+  } else if (userChoice == secretNumber) {
+    document.getElementById("result").style.display = 'flex';
+    document.querySelector("h2").innerHTML = `Acertou em ${numberOfAttempts} tentativas!`;
+    document.addEventListener('keydown', function (e) {
+      if (e.key == 'Enter') {
+        playAgain()
+      }
+    })
+  } else {
+    numberOfAttempts++
+    userChoice = " " + userChoice;
+    failedAttempts.push(userChoice);
+    document.querySelector("h3").style.opacity = '1';
+    document.getElementById("attempts").innerHTML = failedAttempts;
+    document.getElementById("attempts").style.opacity = '1';
+    setTimeout(function () {
+      document.querySelector("h3").style.opacity = '0';
+      document.querySelector("input").value = "";
+      document.querySelector("input").focus();
+    }, 1500);
+  }
+}
+
+
+function playAgain() {
+  document.location.reload(true);
+}
+
+
+
